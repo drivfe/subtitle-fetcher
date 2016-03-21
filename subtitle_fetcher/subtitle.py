162a88b9316b3ef55ab59d3e9d15919742dfb208
drivfe <ivanfelicianox@gmail.com>
2016-03-21 17:15:46 -0400

    python3, rewrite

--- subtitle_fetcher/subtitle.py
+++ subtitle_fetcher/subtitle.py
@@ -1,8 +1,15 @@
-import urllib2
-import urllib
+from __future__ import print_function
 import os
 import hashlib
 import sys
+import requests
+
+USER_AGENT = 'SubDB/1.0 (ajayrfhp/0.1; https://github.com/ajayrfhp/subtitle-fetcher)'
+BASE_URL = 'http://api.thesubdb.com/?'
+CONTENT = {
+    'action': 'download',
+    'language': 'en'
+}
 
 def get_hash(name):
     readsize = 64 * 1024
@@ -13,29 +20,41 @@ def get_hash(name):
         data += f.read(readsize)
     return hashlib.md5(data).hexdigest()
 
-def main():
-    user_agent = 'SubDB/1.0 (ajayrfhp/0.1; https://github.com/ajayrfhp/experiments-with-python--D)'
-    movie_path = sys.argv[1]
-    moviename = os.path.join(os.getcwd(), movie_path)
-    language = 'en'
-    action = 'download'
-    base_url = 'http://api.thesubdb.com/?'
-    hashed = get_hash(moviename)
+def get_subtitle(path):
+    hashed = get_hash(path)
+    content = CONTENT
+    content.update({'hash': hashed})
 
-    content = {
-        'action': action,
-        'hash': hashed,
-        'language': language,
-    }
+    headers = {'User-Agent': USER_AGENT}
+    subtitles = requests.get(BASE_URL, params=content, headers=headers).content
 
-    url = base_url + urllib.urlencode(content)
-    req = urllib2.Request(url)
-    req.add_header('User-Agent', user_agent)
-    res = urllib2.urlopen(req)
-    subtitles = res.read()
-
-    index = moviename.rfind('.')
-    file_name = moviename[0:index] + '.srt'
-    with open(file_name, 'w') as f:
+    file_name = os.path.splitext(path)[0] + '.srt'
+    with open(file_name, 'wb') as f:
         f.write(subtitles)
-    print "Downloaded!"
+
+    print('Subtitles for {} downloaded.'.format(os.path.basename(path)))
+
+def main(path=None):
+    args = ' '.join(sys.argv[1:])
+    files = []
+    extensions = ['.mp4', '.mkv', '.flv', '.wmv', '.avi']
+
+    if len(sys.argv) < 2:
+        args = os.getcwd()
+
+    if os.path.isdir(args) and os.path.exists(args):
+        files = [os.path.join(args, f) for f in os.listdir(args) if os.path.splitext(f)[1] in extensions]
+        if len(files) < 1:
+            sys.exit('No video files found in {}.'.format(args))
+
+    if os.path.isfile(args) and os.path.exists(args):
+        files = [args]
+        
+    if len(files) < 1:
+        sys.exit('File {} does not exist.'.format(args))
+
+    for f in files:
+        subtitle = get_subtitle(f)
+
+if __name__ == '__main__':
+    main()
